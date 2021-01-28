4cb2e50979a72e39b5699da667a241381cba3c10
joyceyskwon <joyceyskwon@gmail.com>
2021-01-28 02:50:12 -0500

    seeded fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "destroy users"
+User.destroy_all
+puts "creating users"
+User.create!([{
+  email: 'joyceyskwon@gmail.com',
+  password: "1234",
+  username: Faker::Games::Fallout.character,
+  first_name: 'Joyce',
+  last_name: 'Kwon',
+  position: 'Engineer',
+  location: 'New York',
+},
+{
+  email: Faker::Name.unique.first_name.downcase.strip + '@gmail.com',
+  password: "1234",
+  username: Faker::Games::Fallout.character,
+  first_name: Faker::Name.unique.first_name,
+  last_name: Faker::Name.unique.last_name,
+  position: Faker::Job.field,
+  location: Faker::Nation.capital_city,
+},
+{
+  email: Faker::Name.unique.first_name.downcase.strip + '@gmail.com',
+  password: "1234",
+  username: Faker::Games::Fallout.character,
+  first_name: Faker::Name.unique.first_name,
+  last_name: Faker::Name.unique.last_name,
+  position: Faker::Job.field,
+  location: Faker::Nation.capital_city,
+}])
+puts "created users"
