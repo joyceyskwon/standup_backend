0720ac4ea721d035431b31c9b5b1d225e88f1aef
joyceyskwon <joyceyskwon@gmail.com>
2021-01-28 20:53:04 -0500

    added user and post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespase :v1 do
+      resources :users
+      resources :posts
+    end
+  end
 end
