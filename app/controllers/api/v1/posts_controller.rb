01c457bbc41ce2bc6c53a05bd12d488cd10fc74a
joyceyskwon <joyceyskwon@gmail.com>
2021-01-28 21:08:44 -0500

    added RESTful methods in user and post controllers

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::PostsController < ApplicationController
+  def index
+   @posts = Post.all
+   render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.create(post_params)
+    render json: @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :content)
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      user_id = encode_token(@user.id)
+      render json: UserSerializer.new(@user)
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username, :first_name, :last_name, :position, :location)
+  end
 end
