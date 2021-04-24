9a72427fa3fefe3a8b57c10a8ecfa491074cf1ef
joyceyskwon <joyceyskwon@gmail.com>
2021-04-23 22:07:23 -0400

    added devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api do
     namespace :v1 do
+      devise_for :users, controllers: { sessions: 'admin/sessions' }
       resources :users
       resources :posts
+
+      root 'home#index'
     end
   end
 end
